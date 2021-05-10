8758a6767c430c4983a6e835a24f4863c96eaa03
Sebastian Zavadschi <szavadschi@gmail.com>
2021-05-10 16:58:35 -0400

    Update curl_http_code.sh

--- curl_http_code.sh
+++ curl_http_code.sh
@@ -23,13 +23,14 @@ colors=(\
 
 until [ "${curl_http_code}" == "${desired_http_code}" ]; do
     curdate=`date "+%D %T"`
-    curl_http_code=`curl -I -A "curls.sh web check (by sz)" -sL --connect-timeout 3 -w "%{http_code}\n" "${url}" -o /dev/null`
+    curl_http_code=`curl --connect-timeout 1 -I -A "curls.sh web check (by sz)" -sL --connect-timeout 3 -w "%{http_code}\n" "${url}" -o /dev/null`
     color="black"
     sleep 1
-    if [ "${curl_http_code}" -ge "100" ] && [ "${curl_http_code}" -lt "200" ]; then status="Status is 1xx"; color="cyan"; fi
-    if [ "${curl_http_code}" -ge "200" ] && [ "${curl_http_code}" -lt "300" ]; then status="Status is 2xx"; color="green"; fi
-    if [ "${curl_http_code}" -ge "300" ] && [ "${curl_http_code}" -lt "400" ]; then status="Status is 3xx"; color="blue"; fi
+    if [ "${curl_http_code}" -ge "100" ] && [ "${curl_http_code}" -lt "200" ]; then status="Status is 1xx"; color="cyan";   fi
+    if [ "${curl_http_code}" -ge "200" ] && [ "${curl_http_code}" -lt "300" ]; then status="Status is 2xx"; color="green";  fi
+    if [ "${curl_http_code}" -ge "300" ] && [ "${curl_http_code}" -lt "400" ]; then status="Status is 3xx"; color="blue";   fi
     if [ "${curl_http_code}" -ge "400" ] && [ "${curl_http_code}" -lt "500" ]; then status="Status is 4xx"; color="yellow"; fi
-    if [ "${curl_http_code}" -ge "500" ] && [ "${curl_http_code}" -lt "600" ]; then status="Status is 5xx"; color="red"; fi
-    echo -en "${colors[${color}]}${curdate} | STAUS CODE: ${curl_http_code} | URL: ${url}" ; tput sgr0; echo ""
+    if [ "${curl_http_code}" -ge "500" ] && [ "${curl_http_code}" -lt "600" ]; then status="Status is 5xx"; color="red";    fi
+    # echo -en "${colors[${color}]}${curdate} | STAUS CODE: ${curl_http_code} | URL: ${url}" ; tput sgr0; echo ""
+    echo -e "${curdate} | STAUS CODE: ${colors[${color}]}${curl_http_code} $(tput sgr0)| URL: ${url}"
 done
